15152ccd115517dad5621edff8f2721ac5e0f6a0
Thomas-Green-II <109981042+Thomas-Green-II@users.noreply.github.com>
2022-07-28 09:48:42 -0400

    Update CodeWars.py

--- CodeWars.py
+++ CodeWars.py
@@ -26,3 +26,16 @@ def basic_op(operator, value1, value2):
         return(value1 / value2)
     else:
         return("Please enter a new operator and numbers")
+
+    
+    
+    
+    
+# Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral 
+#perfect square is an integer n such that sqrt(n) is also an integer.
+# If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.    
+
+def find_next_square(sq):
+    import math as m
+    
+    return ((m.sqrt(sq) + 1)**2) if m.sqrt(sq).is_integer() else -1
