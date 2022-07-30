d433b7d0ebe5e680cafe43b180ece65fb16f72ed
Thomas-Green-II <109981042+Thomas-Green-II@users.noreply.github.com>
2022-07-29 21:24:38 -0500

    Update CodeWars.py

--- CodeWars.py
+++ CodeWars.py
@@ -107,3 +107,8 @@ def filter_list(l):
 # Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers
 def sum_mix(arr):
     return sum(int(x) for x in arr)
+
+
+# You receive an array with your peers' test scores. Now calculate the average and compare your score!
+def better_than_average(class_points, your_points):
+    return your_points > sum(class_points) / len(class_points)
