28d617d2e3b44f41ca1a612a4697b869d9228170
Thomas-Green-II <109981042+Thomas-Green-II@users.noreply.github.com>
2022-07-29 21:06:31 -0500

    Update CodeWars.py

--- CodeWars.py
+++ CodeWars.py
@@ -103,3 +103,7 @@ def filter_list(l):
         if type(x) != str:
             new_list.append(x)
     return new_list
+
+# Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers
+def sum_mix(arr):
+    return sum(int(x) for x in arr)
