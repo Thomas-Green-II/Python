15cd62655f2995a3d8acea136d67af44d4826a19
Thomas-Green-II <109981042+Thomas-Green-II@users.noreply.github.com>
2022-07-31 17:13:31 -0500

    Update CodeWars.py

--- CodeWars.py
+++ CodeWars.py
@@ -138,8 +138,14 @@ def grow(arr):
         num = num * x
     return num
 
-# Given an array of integers, find the one that appears an odd number of times
+## Given an array of integers, find the one that appears an odd number of times
 def find_it(seq):
     for i in seq:
         if seq.count(i)%2!=0:
             return i
+
+        
+# return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters
+def get_middle(s):
+return s[(len(s)-1)//2:(len(s)+2)//2]
+
