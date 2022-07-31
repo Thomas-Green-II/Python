a5bc130230d2da834eb8b938ea3bb5ac32d63132
Thomas-Green-II <109981042+Thomas-Green-II@users.noreply.github.com>
2022-07-31 17:04:23 -0500

    Update CodeWars.py

--- CodeWars.py
+++ CodeWars.py
@@ -137,3 +137,9 @@ def grow(arr):
     for x in arr:
         num = num * x
     return num
+
+# Given an array of integers, find the one that appears an odd number of times
+def find_it(seq):
+    for i in seq:
+        if seq.count(i)%2!=0:
+            return i
