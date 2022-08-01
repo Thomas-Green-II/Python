46b1ad840d843d4730c65cd0a233844d86ccb2ff
Thomas-Green-II <109981042+Thomas-Green-II@users.noreply.github.com>
2022-07-31 21:07:26 -0500

    Update CodeWars.py

--- CodeWars.py
+++ CodeWars.py
@@ -168,3 +168,13 @@ def isValidWalk(walk):
         len(walk) == 10):
             return True
     return False
+
+
+# Given the triangle of consecutive odd numbers:
+#             1
+#          3     5
+#       7     9    11
+#   13    15    17    19
+# 21    23    25    27    29
+def row_sum_odd_numbers(n):
+    return n ** 3
